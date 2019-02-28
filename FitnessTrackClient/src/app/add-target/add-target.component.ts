c7a3d4cd7c1d78367c861991d59d6e65de048307
dmp5658 <dmp5658@psu.edu>
2019-02-28 12:22:19 -0500

    Added target backend and frontend

--- FitnessTrackClient/src/app/add-target/add-target.component.ts
+++ FitnessTrackClient/src/app/add-target/add-target.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {InfoService} from '../info.service';
+import {AuthService} from '../auth.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-target',
   templateUrl: './add-target.component.html',
   styleUrls: ['./add-target.component.css']
 })
-export class AddTargetComponent implements OnInit {
+export class AddTargetComponent {
 
-  constructor() { }
+  constructor(private infoservice: InfoService,
+              private authservice: AuthService) { }
 
-  ngOnInit() {
+  addtargetForm = new FormGroup({
+    targbench: new FormControl('', [Validators.required, Validators.pattern('\\d+\\.?\\d*')]),
+    targsquat: new FormControl('', [Validators.required, Validators.pattern('\\d+\\.?\\d*')]),
+    targcurl: new FormControl('', [Validators.required, Validators.pattern('\\d+\\.?\\d*')]),
+    targlegpress: new FormControl('', [Validators.required, Validators.pattern('\\d+\\.?\\d*')]),
+    targweight: new FormControl('', [Validators.required, Validators.pattern('\\d+\\.?\\d*')]),
+    userid: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.addtargetForm.value);
+    console.log(this.authservice.getUserId);
+    this.addtargetForm.patchValue({
+      userid: this.authservice.getUserId
+    });
+
+    console.log(this.addtargetForm.value);
+    this.infoservice.createTargetEntry(this.addtargetForm.value).subscribe((data) =>
+      console.log('Data - ', data));
   }
+
 
 }
